7c65bd53ddd5c2681ab8f5b9f7a5f12ffebafa1f
Matthew <Matthewevuong@gmail.com>
2025-05-24 20:34:18 -0400

    full of importing path errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeView from "../src/Views/HomeView";
+import RegisterView from "../src/Views/RegisterView";
+import LoginView from "../src/Views/LoginView";
+import MoviesView from "../src/Views/MoviesView";
+import GenreLogin from "../src/Views/GenreLogin"
+import DetailsView from "../src/Views/DetailsView";
+import ErrorView from "../src/Views/ErrorView";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/movies" element={<MoviesView />}>
+          <Route path="genre" element={<GenreLogin />} />
+          <Route path=":id" element={<DetailsView />} />
+        </Route>
+        <Route path = "*" element={<ErrorView />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
